367ff7e293d4636a3b5bfc42b60b08d9e9221855
kvvivvic <rnjs9337@gmail.com>
2023-01-14 23:57:51 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import Videos from "./pages/Videos";
+import VideoDetail from "./pages/VideoDetail";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import NotFound from "./pages/NotFound";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Videos /> },
+      { path: "videos", element: <Videos /> },
+      { path: "videos/:keyword", element: <Videos /> },
+      { path: "videos/watch/:videoId", element: <VideoDetail /> },
+    ],
+  },
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
---
+++ src/pages/Videos.jsx
@@ -0,0 +1,32 @@
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import { useParams } from "react-router-dom";
+import VideoCard from "../components/VideoCard";
+import axios from "axios";
+
+const Videos = () => {
+  const { keyword } = useParams();
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery(["videos", keyword], async () => {
+    return axios.get(`/videos/${keyword ? "search" : "popular"}.json`).then((res) => res.data.items);
+  });
+  return (
+    <>
+      <div>Videos {keyword ? `🔎${keyword}` : "🔥"}</div>
+      {isLoading && <p>Loading... </p>}
+      {error && <p>Something is wrong 😿</p>}
+      {videos && (
+        <ul>
+          {videos.map((video) => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </ul>
+      )}
+    </>
+  );
+};
+
+export default Videos;
