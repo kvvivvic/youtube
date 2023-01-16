52938e0efbd082725250fa0474ae6490da2a01f5
kvvivvic <rnjs9337@gmail.com>
2023-01-16 18:18:37 +0900

    add channelInfo

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import { format, register } from "timeago.js";
 import koLocale from "timeago.js/lib/lang/ko";
 import { formatAgo } from "../util/data";
+import { useNavigate } from "react-router-dom";
 
 register("ko", koLocale);
 const VideoCard = ({ video }) => {
   const { title, thumbnails, channelTitle, publishedAt } = video.snippet;
+  const navigate = useNavigate();
   return (
-    <li>
+    <li
+      onClick={() => {
+        navigate(`videos/watch/${video.id}`, {
+          state: {
+            video,
+          },
+        });
+      }}
+    >
       <img className="w-full" src={thumbnails.medium.url} alt={title} />
       <div>
         <p className="font-semibold my-2 line-clamp-2">{title}</p>
--- src/context/YoutubeApiContext.jsx
+++ src/context/YoutubeApiContext.jsx
@@ -1,13 +1,13 @@
 import { createContext, useContext } from "react";
-import FakeYoutube from "../api/fakeYoutube";
-import FakeYoutubeClient from "../api/fakeYoutubeClient";
+import FakeYoutubeClient from "../api/fakeYotubeClient";
 import Youtube from "../api/youtube";
 import YoutubeClient from "../api/youtubeClient";
 
 export const YoutubeApiContext = createContext();
 
 const client = new FakeYoutubeClient();
-const youtube = new FakeYoutube(client); //new Youtube();
+// const client = new YoutubeClient();
+const youtube = new Youtube(client);
 
 export function YoutubeApiProvider({ children }) {
   return <YoutubeApiContext.Provider value={{ youtube }}>{children}</YoutubeApiContext.Provider>;
